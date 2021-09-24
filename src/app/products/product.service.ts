7d34afef42f9989b996fa656237b9e6c1fe3ca75
Dexter633 <dexter633@apples-MacBook-Pro.local>
2021-09-24 09:27:34 +0700

    initial commit

---
+++ src/app/products/product.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { IProduct } from './product';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private productUrl = 'api/products/products.json';
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(this.productUrl).pipe(
+      tap((data) => console.log('ALL', JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+
+  getProduct(id: number): Observable<IProduct | undefined> {
+    return this.getProducts().pipe(
+      map((products: IProduct[]) => products.find((p) => p.productId === id))
+    );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An Error Occurred: ${err.error.message}`;
+    } else {
+      errorMessage = `Sever returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
+}
---
+++ src/app/shared/star.component.ts
@@ -0,0 +1,26 @@
+import {
+  Component,
+  Input,
+  EventEmitter,
+  OnChanges,
+  Output,
+} from '@angular/core';
+
+@Component({
+  selector: 'pm-star',
+  templateUrl: './star.component.html',
+  styleUrls: ['./star.component.css'],
+})
+export class StarComponent implements OnChanges {
+  @Input() rating: number = 0;
+  cropWidth: number = 75;
+  @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
+
+  ngOnChanges(): void {
+    this.cropWidth = (this.rating * 75) / 5;
+  }
+
+  onClick(): void {
+    this.ratingClicked.emit(`The ratng ${this.rating} was clicked`);
+  }
+}
